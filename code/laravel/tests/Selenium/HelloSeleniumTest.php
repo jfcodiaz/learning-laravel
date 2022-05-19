a253f19c0ba08d29f1bcc939fc98377330e4fa53
JFcoDiaz <jfcodiaz@gmail.com>
2022-05-18 23:50:39 -0500

    Hello selenium

--- code/laravel/tests/Selenium/HelloSeleniumTest.php
+++ code/laravel/tests/Selenium/HelloSeleniumTest.php
@@ -1,6 +1,21 @@
 <?php
+namespace Tests\Selenium;
 
-class HelloSeleniumTest
+class HelloSeleniumTest extends SeleniumTestCase
 {
-
+    public function test_hello_selenium(): void
+    {
+        $this->goTo('http://www');
+        $this->assertElementText('Log in', '#loginBtn')
+            ->click();
+        $this->fillInputById('email', 'admin@servlocal.com');
+        $this->fillInputById('password', 'Password');
+        $this->click('button[type="submit"]');
+        $this->assertElementText('These credentials do not match our records.', 'li');
+        $inputEmail = $this->getById('email');
+        $this->assertSame('admin@servlocal.com', $inputEmail->getAttribute('value'));
+        $this->fillInputById('password', 'unodostres');
+        $this->click('button[type="submit"]');
+        $this->assertElementText('Dashboard', 'h2');
+    }
 }
--- code/laravel/tests/Selenium/SeleniumTestCase.php
+++ code/laravel/tests/Selenium/SeleniumTestCase.php
@@ -1,6 +1,75 @@
 <?php
+namespace Tests\Selenium;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\RemoteWebElement;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Tests\TestCase;
 
-class SeleniumTestCase
+class SeleniumTestCase extends TestCase
 {
+    private RemoteWebDriver $webDriver;
 
+    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $url = 'http://selenium-hub:4444';
+        $this->webDriver = RemoteWebDriver::create(
+            $url,
+            DesiredCapabilities::chrome()
+        );
+    }
+
+    protected function goTo($url) {
+        $this->webDriver->get($url);
+    }
+
+    protected function  fillInputById($id, $value): ?RemoteWebElement
+    {
+        $element = $this->getById($id);
+        $element->sendKeys($value);
+        return $element;
+    }
+
+    protected function click($cssSelector): ?RemoteWebElement
+    {
+        $element = $this->getByCss($cssSelector);
+        $element->click();
+        return $element;
+    }
+
+    protected function assertElementText($expectedText, $cssSelector): ?RemoteWebElement
+    {
+        $element  = $this->getByCss($cssSelector);
+        $this->assertSame($expectedText, $element->getText() );
+        return $element;
+    }
+
+    protected function getWebDriver(): WebDriver {
+        return $this->webDriver;
+    }
+
+    protected function getById(string $id): ?RemoteWebElement{
+        return $this->webDriver->findElement(WebDriverBy::id($id));
+    }
+
+    protected function getByCss(string $cssSelector): ?RemoteWebElement
+    {
+        return $this->webDriver->findElement(
+            WebDriverBy::cssSelector($cssSelector)
+        );
+    }
+
+    protected function getByTagName(string $tagName): ?RemoteWebElement
+    {
+        return $this->webDriver->findElement(
+            WebDriverBy::tagName($tagName)
+        );
+    }
+
+    public function __destruct()
+    {
+        $this->webDriver->quit();
+    }
 }
